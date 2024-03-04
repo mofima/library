05d262588848d0bfcced41a1868aea4061dd71d9
jimogeto <jimogeto@gmail.com>
2024-03-04 11:25:44 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ python manage.py migrate
 echo "Checking if CREATE_SUPERUSER is set..."
 if [ "$CREATE_SUPERUSER" ]; then
     echo "CREATE_SUPERUSER is set. Proceeding to create superuser..."
-    python world_champ_2022/manage.py createsuperuser --no-input
+    python manage.py createsuperuser --no-input
     echo "Superuser creation complete."
 else
     echo "CREATE_SUPERUSER is not set. Skipping superuser creation."
