430022825a9a0da53684ad0338c7064dd917294a
jimogeto <jimogeto@gmail.com>
2024-03-04 11:23:53 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # exit on error
 set -o errexit
+
+# Print Bash version
+echo "Bash Version: $BASH_VERSION"
 
 pip install -r requirements.txt
 
@@ -8,7 +11,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate 
 
 echo "Checking if CREATE_SUPERUSER is set..."
-if [[ $CREATE_SUPERUSER ]]; then
+if [ "$CREATE_SUPERUSER" ]; then
     echo "CREATE_SUPERUSER is set. Proceeding to create superuser..."
     python world_champ_2022/manage.py createsuperuser --no-input
     echo "Superuser creation complete."
